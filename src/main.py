2961a941f8eb334f187ea68ff0f2912fe5ac6bb4
Aarav Sharma <aaravsh2005@gmail.com>
2025-06-11 03:17:50 -0600

    Simple flapping + ground movement finished

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ from pygame.examples.grid import WINDOW_HEIGHT
 
 pygame.init()
 # TODO: dpi scaling issue
+# TODO convert alpha
 # import ctypes
 # try:
 #     ctypes.windll.shcore.SetProcessDpiAwareness(1)  # PROCESS_SYSTEM_DPI_AWARE = 1
@@ -18,7 +19,6 @@ pygame.init()
 def get_asset_path(asset_name): # Platform neutral
     return os.path.join("../assets", asset_name)
 BG_IMG = pygame.transform.scale2x(pygame.image.load(get_asset_path("background.png")))
-GROUND_IMG = pygame.transform.scale2x(pygame.image.load(get_asset_path("ground.png")))
 #TODO: allow rescalablity
 WIN_WIDTH = 500
 WIN_HEIGHT = 800
@@ -28,6 +28,9 @@ class Bird(pygame.sprite.Sprite):
     FRAME_1 = pygame.transform.scale2x(pygame.image.load(get_asset_path("bird_frame_1.png")))
     FRAME_2 = pygame.transform.scale2x(pygame.image.load(get_asset_path("bird_frame_2.png")))
     FRAME_3 = pygame.transform.scale2x(pygame.image.load(get_asset_path("bird_frame_3.png")))
+    FLAP_STRENGTH = 18
+    FLAP_TILT = 25
+    NEGATIVE_TILT_EXPO = 1.24
 
     def __init__(self, x, y):
         super().__init__()
@@ -35,56 +38,103 @@ class Bird(pygame.sprite.Sprite):
         self.x = x
         self.y = y
 
-        self.frames = [Bird.FRAME_1, Bird.FRAME_2, Bird.FRAME_3]
+        self.frames = [Bird.FRAME_1.convert_alpha(), Bird.FRAME_2.convert_alpha(), Bird.FRAME_3.convert_alpha()]
         self.animation_index = 0
+        self.tilt = 0
 
         self.image = self.frames[self.animation_index]
         self.rect = self.image.get_rect(center = (x, y))
         self.gravity = 0
 
-    def get_player_input(self):
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_SPACE]:
-            print("Flap")
-            # TODO sounds
-            self.gravity = -20
+    def get_player_input(self, events):
+        # TODO sound
+        for event in events:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                self.flap()
 
+    def flap(self):
+        self.gravity = -1 * Bird.FLAP_STRENGTH
     def apply_gravity(self):
         self.rect.y += self.gravity
         self.gravity += 1
+        # print(self.gravity)
 
-    def update(self):
-        self.get_player_input()
+    def animate(self):
+        if self.gravity < 0: # Head tilt up while going upwards
+            self.tilt = Bird.FLAP_TILT
+        else: # Falling downwards, exponentially rotate
+            # Tilt based on gravity
+            self.tilt = -1 * self.gravity**(Bird.NEGATIVE_TILT_EXPO)
+            if self.tilt < -90:
+                self.tilt = -90
+
+        self.image = pygame.transform.rotate(self.frames[self.animation_index], self.tilt) # Do not repeatedly rotate same image
+        self.rect = self.image.get_rect(center = self.rect.center)
+    def update(self, events):
+        self.get_player_input(events)
         self.apply_gravity()
+        self.animate()
         # Animate
 
 
+class Ground(pygame.sprite.Sprite):
+    VELOCITY = -5
+    Y_POS = 710
+    GROUND_IMG = pygame.transform.scale2x(pygame.image.load(get_asset_path("ground.png")))
 
+    def __init__(self, x):
+        super().__init__()
+
+        self.x = x
+        self.y = Ground.Y_POS
+
+        self.image = Ground.GROUND_IMG.convert()
+        self.rect = self.image.get_rect(topleft = (self.x, self.y))
+
+
+    def move(self):
+        self.rect.x += Ground.VELOCITY
+
+        if self.rect.right <= 0:
+            self.rect.x += self.image.get_width() * 2 # Move it over the other image
+
+    def update(self):
+        self.move()
 
 
 
 
 def main():
     screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+    BG_IMG.convert()
     player_group = pygame.sprite.GroupSingle()
     player_group.add(Bird(200, 200))
+    ground_group = pygame.sprite.Group(
+    Ground(0),
+        Ground(Ground.GROUND_IMG.get_width())
+    )
     clock = pygame.time.Clock() # Will help limit framerate for consistency
 
-    # Timer
 
     while True:
-        for event in pygame.event.get():
+        events = pygame.event.get()
+        for event in events:
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
+            # if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            #     player_group.sprite.gravity = -20
 
 
 
         screen.blit(BG_IMG, (0, -100))
-        screen.blit(GROUND_IMG, (0, 710))
+        # screen.blit(GROUND_IMG, (0, 710))
 
-        player_group.update()
+        player_group.update(events)
         player_group.draw(screen)
+
+        ground_group.update()
+        ground_group.draw(screen)
 
         pygame.display.update()
         clock.tick(60) # Setting max framerate
